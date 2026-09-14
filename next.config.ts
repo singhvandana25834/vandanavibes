daaeddef6e054e0933448cfd5b3d50bb11fac4ca
vandana_kumari <143302180+singhvandana25834@users.noreply.github.com>
2026-09-14 17:59:21 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,16 @@
+
+import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: false,
+  register: true,
+});
+
+const nextConfig: NextConfig = {
+  turbopack: {},
+};
+
+export default withPWA(nextConfig);
+
